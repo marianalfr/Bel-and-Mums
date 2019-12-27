d6607bc5804f644e5e61a5f3b480223264c2ec7c
marianalfr <marianalfr@gmail.com>
2019-12-27 18:59:31 +0100

    Initial commit

---
+++ j/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#slides').superslides({
+     slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: true,
+    scrollable: true,
+
+
+  });
+});
